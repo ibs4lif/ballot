e89e62fdf7f34697daa0abc097f9ced74e41a0b6
Ibrahima Sarr <ibrahimasarr88@gmail.com>
2016-03-21 13:04:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongojs = require('mongojs');
-var db = mongojs('ibrahima:sarr@ds011168.mlab.com:11168/ballot', ['magasin','employe']);
+var db = mongojs('ibrahima:sarr@ds011168.mlab.com:11168/ballot', ['magasin','employe','facture']);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -17,6 +17,13 @@ app.get('/', function(req,res){
 
 app.get('/employe', function(req,res){
 	db.employe.find(function(err,docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.get('/facture', function(req,res){
+	db.facture.find(function(err,docs){
 		console.log(docs);
 		res.json(docs);
 	});
