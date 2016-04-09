bbe559666f5b64d5c7bec5659d675be77a1184a8
Ibrahima Sarr <ibrahimasarr88@gmail.com>
2016-04-08 22:21:08 -0400

    Update facture.js

--- models/facture.js
+++ models/facture.js
@@ -2,7 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var FactueSchema = new Schema({
-	items:Array
+	remorque:String,
+	route:String,
+	porte:String,
+	reference:String,
+	items:Array,
+	date:String,
 });
 
 module.exports = mongoose.model('facture',FactueSchema);
